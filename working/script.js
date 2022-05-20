f2ccc763cc8ed45a07ab32bec0e1410e6f71c5e7
Ebenezhar Selvakumar <103163165+Ebenezhar@users.noreply.github.com>
2022-05-20 21:56:20 +0530

    Update01

---
+++ working/script.js
@@ -0,0 +1,32 @@
+
+        let count = 10;
+        setTimeout(()=>{
+            document.querySelector(".counter").innerText = --count;
+            setTimeout(()=>{
+                document.querySelector(".counter").innerText = --count;
+                setTimeout(()=>{
+                    document.querySelector(".counter").innerText = --count;
+                    setTimeout(()=>{
+                       document.querySelector(".counter").innerText = --count;
+                        setTimeout(()=>{
+                            document.querySelector(".counter").innerText = --count;
+                            setTimeout(()=>{
+                                document.querySelector(".counter").innerText = --count;
+                                setTimeout(()=>{
+                                    document.querySelector(".counter").innerText = --count;
+                                    setTimeout(()=>{
+                                        document.querySelector(".counter").innerText = --count;
+                                        setTimeout(()=>{
+                                            document.querySelector(".counter").innerText = --count;
+                                            setTimeout(()=>{
+                                                document.querySelector(".counter").innerText = "Happy Independence Day";
+                                            },1*1000);
+                                        },1*1000);
+                                    },1*1000);
+                                },1*1000);
+                            },1*1000);
+                        },1*1000);
+                    },1*1000);
+                },1*1000);
+            },1*1000);
+        },1*1000);
